18b88b583b8c980311a5423107d60857dcb382d5
glowman554 <glowman434@gmail.com>
2021-04-27 23:45:10 +0200

    fix

--- kernel/main.c
+++ kernel/main.c
@@ -3,8 +3,8 @@ void _start(void) {
 	char* video = (char*) 0xb8000;
 
 	for (int i = 0; i < 25 * 80; i++) {
-		video[2*i] = ' ';
-		video[2*i+1] = 0x07;
+		video[2 * i] = ' ';
+		video[2 * i + 1] = 0x07;
 	}
 
 
